eb7b1f4ae235f62363f3607148ae32920d648092
jaseemuddin <njaseemuddin@gmail.com>
2024-08-13 14:03:48 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,61 @@
-import { Route, Router, Routes, useNavigate } from "react-router-dom";
+// import { Route, Router, Routes, useNavigate } from "react-router-dom";
+// import "./App.css";
+// import About from "./components/About/About";
+// import Contact from "./components/Contact/Contact";
+// import Hero from "./components/Hero/Hero";
+// import Navbar from "./components/Navbar/Navbar";
+// import Register from "./components/Register/Register";
+// import Sponsors from "./components/Sponsors/Sponsors";
+// import Links from "./components/Links/Links";
+
+// function App() {
+
+//   return (
+//     <>
+//       <Navbar />
+//       <Hero />
+//       <About />
+//       <Register />
+//       <Sponsors />
+//       <Contact />
+//       <Routes>
+//         <Route path="/links" element={<Links />} />
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
-import Register from "./components/Register/Register";
-import Sponsors from "./components/Sponsors/Sponsors";
-import Links from "./components/Links/Links";
-// import useIntersectionObserver from "./IntersectionObserver";
-// import { useState } from "react";
+import { Suspense, lazy } from "react";
+import SkeletonCard from "./components/Skeleton/SkeletonCard";
+
+// Lazy load components
+const About = lazy(() => import("./components/About/About"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
+const Register = lazy(() => import("./components/Register/Register"));
+const Sponsors = lazy(() => import("./components/Sponsors/Sponsors"));
 
 function App() {
-  // const navigate = useNavigate();
-  // const [activeSection, setActiveSection] = useState("");
-
-  // useIntersectionObserver((section) => {
-  //   setActiveSection(section);
-  //   navigate(`#${section}`, { replace: true });
-  // });
   return (
     <>
-      {/* <Navbar />
-      <section id="hero">
-        <Hero />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="register">
-        <Register />
-      </section>
-      <section id="sponsors">
-        <Sponsors />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-      <Routes>
-        <Route path="/links" element={<Links />} />
-      </Routes> */}
-
       <Navbar />
       <Hero />
-      <About />
-      <Register />
-      <Sponsors />
-      <Contact />
-      <Routes>
-        <Route path="/links" element={<Links />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div>
+            <SkeletonCard />
+          </div>
+        }
+      >
+        <About />
+        <Register />
+        <Sponsors />
+        <Contact />
+      </Suspense>
     </>
   );
 }
